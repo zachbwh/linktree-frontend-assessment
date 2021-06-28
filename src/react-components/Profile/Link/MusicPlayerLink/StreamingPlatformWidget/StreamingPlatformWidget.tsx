2b58d2cb858b8066ad7e87b5a372ef1d96209a9a
Zach Huxford <zachbwh@gmail.com>
2021-06-28 20:51:48 +1200

    streaming platform iframes lazy load as by default they are hidden from the UI so should not hinder the loading of other content

--- src/react-components/Profile/Link/MusicPlayerLink/StreamingPlatformWidget/StreamingPlatformWidget.tsx
+++ src/react-components/Profile/Link/MusicPlayerLink/StreamingPlatformWidget/StreamingPlatformWidget.tsx
@@ -8,10 +8,10 @@ const StreamingPlatformWidget: React.FunctionComponent<{ streamingPlatformSong: 
 
     switch(props.streamingPlatformSong.Platform) {
         case "spotify":
-            streamingPlatformWidget = <iframe title="Spotify Player" src={props.streamingPlatformSong.AudioPlayerSongLink} width="100%" height="80" frameBorder="0" allowTransparency={true} allow="encrypted-media"></iframe>;
+            streamingPlatformWidget = <iframe title="Spotify Player" src={props.streamingPlatformSong.AudioPlayerSongLink} loading="lazy" width="100%" height="80" frameBorder="0" allowTransparency={true} allow="encrypted-media"></iframe>;
         break;
         case "apple-music":
-            streamingPlatformWidget = <iframe title="Apple Music Player" allow="autoplay *; encrypted-media *; fullscreen *" frameBorder="0" width="100%" height="150" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src={props.streamingPlatformSong.AudioPlayerSongLink}></iframe>
+            streamingPlatformWidget = <iframe title="Apple Music Player" allow="autoplay *; encrypted-media *; fullscreen *" frameBorder="0" width="100%" height="150" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src={props.streamingPlatformSong.AudioPlayerSongLink} loading="lazy"></iframe>
         break;
         // @todo add more rendering templates for other music streaming services embed codes
     }
